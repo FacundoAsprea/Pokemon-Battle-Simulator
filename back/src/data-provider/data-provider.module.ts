69dd6c8c3869c816e0aed2e7e931202122503ee8
= <=>
2025-09-01 17:06:43 -0300

    Funcion q brinda la IP

--- back/src/data-provider/data-provider.module.ts
+++ back/src/data-provider/data-provider.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SpritesModule } from './sprites/sprites.module';
 import { PokemonDataModule } from './pokemon-data/pokemon-data.module';
+import { IpModule } from './ip/ip.module';
 
 @Module({
-  imports: [SpritesModule, PokemonDataModule],
+  imports: [SpritesModule, PokemonDataModule, IpModule],
 })
 export class DataProviderModule {}
---
+++ back/src/data-provider/ip/ip.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import os from 'os';
+
+@Injectable()
+export class IpService {
+  getIp(): string | null {
+    const nets = os.networkInterfaces();
+    console.log(nets);
+
+    for (const name of Object.keys(nets)) {
+      for (const net of nets[name] ?? []) {
+        if (net.family == 'IPv4' && !net.internal) return net.address;
+      }
+    }
+
+    return null;
+  }
+}
